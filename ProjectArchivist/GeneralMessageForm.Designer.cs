f2d1605227b3d621cd6f71c97b16b2a871a6b108
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-03-17 15:47:17 -0400

    created general message form

---
+++ ProjectArchivist/GeneralMessageForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ProjectArchivist
+{
+    partial class GeneralMessageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label_Status = new System.Windows.Forms.Label();
+            this.Button_Close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Label_Status
+            // 
+            this.Label_Status.AutoSize = true;
+            this.Label_Status.Location = new System.Drawing.Point(13, 13);
+            this.Label_Status.MaximumSize = new System.Drawing.Size(250, 250);
+            this.Label_Status.Name = "Label_Status";
+            this.Label_Status.Size = new System.Drawing.Size(107, 13);
+            this.Label_Status.TabIndex = 0;
+            this.Label_Status.Text = "Status text goes here";
+            // 
+            // Button_Close
+            // 
+            this.Button_Close.Location = new System.Drawing.Point(104, 76);
+            this.Button_Close.Name = "Button_Close";
+            this.Button_Close.Size = new System.Drawing.Size(75, 23);
+            this.Button_Close.TabIndex = 1;
+            this.Button_Close.Text = "Close";
+            this.Button_Close.UseVisualStyleBackColor = true;
+            this.Button_Close.Click += new System.EventHandler(this.Button_Close_Click);
+            // 
+            // GeneralMessageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 111);
+            this.Controls.Add(this.Button_Close);
+            this.Controls.Add(this.Label_Status);
+            this.Name = "GeneralMessageForm";
+            this.Text = "Status";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label_Status;
+        private System.Windows.Forms.Button Button_Close;
+    }
+}
---
+++ ProjectArchivist/GeneralMessageForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectArchivist
+{
+    public partial class GeneralMessageForm : Form
+    {
+        public GeneralMessageForm(string text)
+        {
+            InitializeComponent();
+            Label_Status.Text = text;
+        }
+
+        private void Button_Close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
